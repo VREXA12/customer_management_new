74172d3318b5f535fe29e28b58319e36cac05752
VREXA12 <pvpranavverma12@gmail.com>
2021-03-15 12:48:17 +0530

    Sprint 1

---
+++ src/test/java/com/cg/apps/customermsnew/CustomermsnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.apps.customermsnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomermsnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
